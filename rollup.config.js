f0de3f96faa30a018061ef4e3b7bb7793372e8ff
gusrb3164 <gusrb3164@gmail.com>
2022-04-04 18:15:25 +0900

    chore: support umd js

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import typescript from "@rollup/plugin-typescript";
 import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import typescript from "rollup-plugin-typescript2";
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"];
 
 export default {
   input: "./src/index.ts",
   plugins: [
-    typescript({ target: "es5", sourceMap: true }),
+    typescript({ plugins: "tsconfig.json", useTsconfigDeclarationDir: true }),
     peerDepsExternal(),
     resolve({ extensions }),
     commonjs({
@@ -17,13 +17,11 @@ export default {
   ],
   output: [
     {
-      sourcemap: true,
       file: "dist/index.esm.js",
       format: "es",
     },
     {
       name: "react-custom-alert",
-      sourcemap: true,
       file: "dist/index.js",
       format: "umd",
     },
