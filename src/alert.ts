6050229b4c3abf3cf151f5a28deacbdf4d7b6135
gusrb3164 <gusrb3164@gmail.com>
2022-09-28 00:58:45 +0900

    remove: legacy alert file

--- src/alert.ts
+++
@@ -1,9 +0,0 @@
-import type { AlertDetail } from "./type";
-
-export const alert = (detail: AlertDetail) => {
-  window.dispatchEvent(
-    new CustomEvent("toastAlert", {
-      detail,
-    })
-  );
-};
